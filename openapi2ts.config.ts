c7df6f4241729ff93b9ab29f58d8f2e08463fc2d
cmhwp <2232706035@qq.com>
2025-08-10 12:40:41 +0800

    feat: update ESLint and add OpenAPI configuration
    - Disable specific ESLint rules for Vue and TypeScript.
    - Introduce openapi2ts.config.ts for generating frontend requests and TypeScript models from backend APIs.
    - Add @iconify/vue as a new dependency and include it in package.json and package-lock.json.
    - Update Vite configuration to include unplugin-vue-components for Ant Design Vue support.

---
+++ openapi2ts.config.ts
@@ -0,0 +1,8 @@
+/**
+ * 根据后端接口生成前端请求和 TS 模型代码
+ */
+export default {
+  requestLibPath: "import request from '@/request'",
+  schemaPath: 'http://localhost:8123/v3/api-docs/default',
+  serversPath: './src',
+}
